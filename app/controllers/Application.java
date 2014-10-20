c4d0eba597b4712fdc664c972bf992f0144f2d94
Jarel <ballsofcurry@gmail.com>
2014-10-20 14:19:30 +0800

    Home page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.Play.*;
 
 import java.util.*;
 
@@ -9,8 +10,18 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() { 	
-        render();
+    public static void index() {
+    	Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+    
 }
