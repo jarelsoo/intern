c789cf71e2c95078273fa04987016bf0146c690b
Jarel <ballsofcurry@gmail.com>
2014-10-20 17:37:24 +0800

    Guide 8

---
+++ app/controllers/Security.java
@@ -0,0 +1,22 @@
+package controllers;
+ 
+import models.*;
+ 
+public class Security extends Secure.Security {
+	
+	static boolean check(String profile) {
+	    if("admin".equals(profile)) {
+	        return User.find("byEmail", connected()).<User>first().isAdmin;
+	    }
+	    return false;
+	}
+	
+	static void onDisconnected() {
+	    Application.index();
+	}
+	
+	static void onAuthenticated() {
+	    Admin.index();
+	}
+    
+}
