0d5ebdfdd0be969ef8217120799f11a1c192219e
Jarel <ballsofcurry@gmail.com>
2014-10-20 15:24:04 +0800

    Guide 4

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.Play.*;
+import play.data.validation.*;
 
 import java.util.*;
 
@@ -17,11 +18,26 @@ public class Application extends Controller {
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
-
+    
     @Before
     static void addDefaults() {
         renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+    
+    public static void show(Long id) {
+        Post post = Post.findById(id);        
+        render(post);
+    }
+    
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
     }
     
 }
